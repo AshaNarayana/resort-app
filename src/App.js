2593fd99d533e21e710ad546037e93b1e93e415f
AshaNarayana <ashahnh@gmail.com>
2020-11-12 13:38:37 +0100

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 //import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey</h1>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/rooms/:singleRoom" exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+export default class NavBar extends Component {
+  render() {
+    return <div>Hello from NavBar page</div>;
+  }
+}
